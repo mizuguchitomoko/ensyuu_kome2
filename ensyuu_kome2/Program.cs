b1a3529fd14b982308ddf3a1d2ce5c54b106078d
shuikouzhizi7@gmail.com <shuikouzhizi7@gmail.com>
2022-06-07 20:56:22 +0900

    プロジェクト ファイルを追加します。

---
+++ ensyuu_kome2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ensyuu_kome2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 4; i++)//*が増えるループを5回
+            {
+                string n = "*";
+                string str = string.Concat(Enumerable.Repeat("**", i));//ループの回数だけ**増加
+                if (i == 1)//1行目は*一つから始めたい
+                {
+                    Console.WriteLine(n);
+                }
+                Console.WriteLine(n+str.ToString());//奇数にするため+nをした
+            }
+        }
+    }
+}
